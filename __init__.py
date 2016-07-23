1ae013cf351182c2100273e953b3068422e8f128
root <marc@fogtest.com>
2016-07-23 05:35:46 +0000

    tested get_list_to_sync()

--- __init__.py
+++ __init__.py
@@ -53,8 +53,34 @@ def get_list_of_comm_items_to_sync(data_dir):
     sync_list = []
     for ci in db_dict:
         if ci not in disk_dict:
-            sync_list.append(int(os.path.basename(ci)).split('.')[1])
+            sync_list.append(int(os.path.basename(ci).split('.')[0]))
 
     return sync_list
 
+"""
 print(get_list_of_comm_items_to_sync('/php-apps/cake.rocketsredglare.com/rrg/data/transactions/invoices/invoice_items/commissions_items/'))
+
+
+<?xml version="1.0"?>
+<invoices-items-commissions-item>
+  <id>6431</id>
+  <employee_id>1479</employee_id>
+  <invoices_item_id>4298</invoices_item_id>
+  <invoice_id>3356</invoice_id>
+  <commissions_report_id>3356</commissions_report_id>
+  <commissions_reports_tag_id>361</commissions_reports_tag_id>
+  <description>xxx dddd - Overtime</description>
+  <date>2012-08-12</date>
+  <percent>38.5</percent>
+  <amount>22.45</amount>
+  <rel_inv_amt>1788.9</rel_inv_amt>
+  <rel_inv_line_item_amt>249.895</rel_inv_line_item_amt>
+  <rel_item_amt>57.7125</rel_item_amt>
+  <rel_item_quantity>4.33</rel_item_quantity>
+  <rel_item_cost>42.75</rel_item_cost>
+  <rel_item_amt>57.7125</rel_item_amt>
+  <cleared>1</cleared>
+  <voided>0</voided>
+  <date_generated>Mon, 11 Jul 2016 11:19:59</date_generated>
+</invoices-items-commissions-item>
+"""
