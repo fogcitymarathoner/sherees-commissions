51e193c57ed0edc34274d22a3ff9755c48082747
marc condon <marc@fogtest.com>
2016-07-22 22:15:40 -0700

    checking in get_list_of_comm_items_to_sync, not tested

--- __init__.py
+++ __init__.py
@@ -10,15 +10,15 @@ from models import engine
 from models import Citem
 
 
-def directory_date_dictionary(dir):
+def directory_date_dictionary(data_dir):
     """
     returns dictionary of a directory in (name: cdate} format
-    :param dir:
+    :param data_dir:
     :return:
     """
     dirFileList = []
-    for dirName, subdirList, fileList in os.walk(dir, topdown=False):
-        if dir == dirName:
+    for dirName, subdirList, fileList in os.walk(data_dir, topdown=False):
+        if data_dir == dirName:
             for f in fileList:
 
                 dirFileList.append(os.path.join(dirName, f))
@@ -26,10 +26,10 @@ def directory_date_dictionary(dir):
     return {f: dt.strptime(time.ctime(os.path.getmtime(f)), DIR_CREATE_TIME_FORMAT) for f in dirFileList}
 
 
-def db_date_dictionary_comm_item(dir):
+def db_date_dictionary_comm_item(data_dir):
     """
     returns database dictionary counter part to dicectory_date_dictionary for sync determination
-    :param dir:
+    :param data_dir:
     :return:
     """
     Session = sessionmaker(bind=engine)
@@ -39,8 +39,22 @@ def db_date_dictionary_comm_item(dir):
     citem_dict = {}
 
     for comm_item in session.query(Citem).order_by(Citem.id):
-        f = os.path.join(dir, '%s.xml' % str(comm_item.id).zfill(5))
+        f = os.path.join(data_dir, '%s.xml' % str(comm_item.id).zfill(5))
         citem_dict[f] = comm_item.last_sync_time
 
     return citem_dict
 
+
+def get_list_of_comm_items_to_sync(data_dir):
+
+    disk_dict = directory_date_dictionary(data_dir)
+    db_dict = db_date_dictionary_comm_item(data_dir)
+
+    sync_list = []
+    for ci in db_dict:
+        if ci not in disk_dict:
+            sync_list.append(int(os.path.basename(ci)).split('.')[1])
+
+    return sync_list
+
+print(get_list_of_comm_items_to_sync('/php-apps/cake.rocketsredglare.com/rrg/data/transactions/invoices/invoice_items/commissions_items/'))
