558f508f005be2c5fc5cfb8c9819361fa2bea0a8
marc condon <marc@fogtest.com>
2016-07-23 11:51:04 -0700

    still working on date corrections

--- __init__.py
+++ __init__.py
@@ -84,7 +84,6 @@ data_dir = '/php-apps/cake.rocketsredglare.com/rrg/data/transactions/invoices/in
 date_dict, citems = db_date_dictionary_comm_item(data_dir)
 disk_dict =directory_date_dictionary(data_dir)
 Session = sessionmaker(bind=engine)
-session = Session()
 for comm_item in citems:
     f = full_comm_item_xml_path(data_dir, comm_item)
     if f not in disk_dict:
@@ -92,9 +91,8 @@ for comm_item in citems:
     else:
         # correct Null modified dates
         if comm_item.modified_date is None:
-            comm_item.modified_date = dt.now()
-            comm_item.save()
 
+            session = Session()
             session.query(Citem).filter_by(id=comm_item.id).update({"modified_date": dt.now()})
             session.commit()
 
