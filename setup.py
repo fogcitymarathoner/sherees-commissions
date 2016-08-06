e5564765504c79b3d85411b8c42714e9d8434cb5
marc condon <marc@fogtest.com>
2016-08-06 20:22:09 +0000

    0.0.125 added tabulate module as dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.124',
+    version='0.0.125',
     description='Utility Library for Taking AR report generating out of CakePHP DB',
     long_description=long_description,
 
@@ -72,7 +72,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['s3_mysql_backup', 'sqlalchemy', 'mysql-python'],
+    install_requires=['s3_mysql_backup', 'sqlalchemy', 
+                      'tabulate', 'mysql-python'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
