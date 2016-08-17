5921939206b7ee1ec9b41e91a2d36d741be30edb
marc condon <marc@fogtest.com>
2016-08-17 10:05:22 -0700

    linting

--- rrg/reports/invoices.py
+++ rrg/reports/invoices.py
@@ -16,8 +16,8 @@ def invoices_year_month(session, args):
         nexty = int(y) + 1
         nextm = 1
 
-    return  session.query(Invoice).filter(and_(Invoice.date >= dt(year=y, month=m, day=1),
-        Invoice.date < dt(year=nexty, month=nextm, day=1))).all()
+    return session.query(Invoice).filter(and_(Invoice.date >= dt(year=y, month=m, day=1),
+                                              Invoice.date < dt(year=nexty, month=nextm, day=1))).all()
 
 
 def active_contracts(session):
