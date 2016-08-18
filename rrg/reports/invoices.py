6d3ff22e3bedf5248c8aa4677857085e4b6c59a7
Marc Condon <marccondon@fb.com>
2016-08-17 17:22:17 -0700

    added filter to strip voided invoices from monthly invoice report

--- rrg/reports/invoices.py
+++ rrg/reports/invoices.py
@@ -18,7 +18,8 @@ def invoices_year_month(session, args):
 
     return session.query(Invoice).filter(
         and_(Invoice.date >= dt(year=y, month=m, day=1),
-             Invoice.date < dt(year=nexty, month=nextm, day=1))).all()
+             Invoice.date < dt(year=nexty, month=nextm, day=1),
+             Invoice.voided == False)).all()
 
 
 def active_contracts(session):
