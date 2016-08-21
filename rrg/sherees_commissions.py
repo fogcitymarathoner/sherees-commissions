b978e798bb9e3c5574f519568a7846bf5dd3ec29
marc condon <marc@fogtest.com>
2016-08-21 16:10:57 -0700

    fixed date sort on notes report

--- rrg/sherees_commissions.py
+++ rrg/sherees_commissions.py
@@ -92,7 +92,7 @@ def sherees_notes_report(session, args):
             notestx = ''.join([i if ord(i) < 128 else ' ' for i in np.notes])
         else:
             notestx = ''
-        new_rec = [np.id, dt.strftime(np.date, '%m/%d/%Y'), notestx, -np.amount, np.check_number]
+        new_rec = [np.id, np.date, notestx, -np.amount, np.check_number]
         logger.debug('adding notepayment %s' % new_rec)
         combined.append(new_rec)
     for n in notes:
@@ -100,7 +100,7 @@ def sherees_notes_report(session, args):
             notestx = ''.join([i if ord(i) < 128 else ' ' for i in n.notes])
         else:
             notestx = ''
-        new_rec = [n.id, dt.strftime(n.date, '%m/%d/%Y'), notestx, n.amount, '']
+        new_rec = [n.id, n.date, notestx, n.amount, '']
         logger.debug('adding note %s' % new_rec)
         combined.append(new_rec)
     combined_sorted = sorted(combined, key=itemgetter(1))
@@ -110,7 +110,7 @@ def sherees_notes_report(session, args):
     logger.debug(combined_sorted)
     res_dict_transposed = {
         'id': [i[0] for i in combined_sorted],
-        'date': [i[1] for i in combined_sorted],
+        'date': [dt.strftime(i[1], '%m/%d/%Y') for i in combined_sorted],
         'description': [i[2] for i in combined_sorted],
         'amount': ["%.2f" % round(i[3], 2) for i in combined_sorted],
         'balance': [i[3] for i in combined_sorted],
