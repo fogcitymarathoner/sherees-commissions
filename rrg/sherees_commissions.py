7a86cb66af84e58f9fef145d41bba2a8714aa435
marc condon <marc@fogtest.com>
2016-08-27 20:09:32 +0000

    server fixex

--- rrg/sherees_commissions.py
+++ rrg/sherees_commissions.py
@@ -398,23 +398,19 @@ def cache_comm_items(session, args):
     # Make sure destination directories exist
     #
     verify_comm_dirs_ready(args.datadir, rel_dir_set)
-
     to_sync = []
     for comm_item in citems:
         if comm_item.amount > 0:
-            file = full_dated_obj_xml_path(args.datadir, comm_item)
+            file = full_dated_comm_item_xml_path(args.datadir, comm_item)
             # add to sync list if comm item not on disk
             if file[0] not in disk_dict:
-                print '%s not on disk' % file[0]
                 to_sync.append(comm_item)
             else:
                 # check the rest of the business rules for syncing
                 # no time stamps, timestamps out of sync
                 if comm_item.last_sync_time is None or comm_item.modified_date is None:
                     to_sync.append(comm_item)
-                    continue
                 if comm_item.modified_date > comm_item.last_sync_time:
-                    print 'caught newer db copy %s' % comm_item
                     to_sync.append(comm_item)
 
     # Write out xml
