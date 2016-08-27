9a911e3008ea2ca3e18502f3b6cb1bd89b907b76
marc condon <marc@fogtest.com>
2016-08-27 14:16:50 -0700

    added cached arg

--- rrg/scripts/commissions.py
+++ rrg/scripts/commissions.py
@@ -22,6 +22,9 @@ parser.add_argument('--mysql-host', required=True, help='database host - MYSQL_P
 parser.add_argument('--mysql-port', required=True, help='database port - MYSQL_PORT_3306_TCP_PORT', default=3306)
 parser.add_argument('--db', required=True, help='d', default='rrg')
 parser.add_argument('--db-pass', required=True, help='database pw', default='deadbeef')
+parser.add_argument('--cache', dest='cache', action='store_true')
+parser.add_argument('--no-cache', dest='cache', action='store_false')
+parser.set_defaults(cache=True)
 
 
 def comm():
