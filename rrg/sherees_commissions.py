2a312e24425e0619596a30dc2134d42bd0f2dcca
Marc Condon <marccondon@fb.com>
2016-08-29 16:20:49 -0700

    still repairing dirs

--- rrg/sherees_commissions.py
+++ rrg/sherees_commissions.py
@@ -717,7 +717,7 @@ def iitem_exclude(session, args):
 
 def invoice_report_month_year(args):
     invdir = os.path.join(args.datadir, 'transactions', 'invoices',
-                          'invoice_items'
+                          'invoice_items',
                           'commissions_items', 'invoices',
                           str(args.year), str(args.month).zfill(2))
     inv_items_dir = os.path.join(args.datadir, 'transactions', 'invoices',
