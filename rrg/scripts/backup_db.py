823325af643049f916146f74ded7e21d8a52d3a2
marc condon <marc@fogtest.com>
2016-09-04 10:10:20 -0700

    cleaned up args to backup db

--- rrg/scripts/backup_db.py
+++ rrg/scripts/backup_db.py
@@ -1,26 +1,23 @@
-
 import argparse
 
-from s3_mysql_backup.backup_db import backup as s3_backup_db
+from s3_mysql_backup.backup_db import backup_db as s3_backup_db
 
-parser = argparse.ArgumentParser(description='S3 DB Bzckup')
+parser = argparse.ArgumentParser(description='S3 DB Backup')
 
-parser.add_argument('project', help='project name', choices=['rrg', 'biz'])
 parser.add_argument('--db-backups-dir', required=True, help='database backups directory',
                     default='/php-apps/cake.rocketsredglare.com/rrg/data/backups/')
 
-parser.add_argument('--db-user', required=True, help='database user', default='marcdba')
-parser.add_argument('--project-name', required=True, help='Project Name', default='rrg')
-parser.add_argument('--s3-folder', required=True, help='S3 Folder', default='rrg')
+parser.add_argument('--s3-folder',  help='S3 Folder', default='')
 parser.add_argument('--bucket-name', required=True, help='Bucket Name', default='php-apps-cluster')
 parser.add_argument('--aws-access-key-id', required=True, help='AWS_ACCESS_KEY_ID', default='rrg')
 parser.add_argument('--aws-secret-access-key', required=True, help='AWS_SECRET_ACCESS_KEY_ID', default='deadbeef')
 parser.add_argument('--backup-aging-time', help='delete backups older than backup-aging-time', default=30)
-parser.add_argument('--db-user', required=True, help='database user', default='marcdba')
 parser.add_argument('--mysql-host', required=True, help='database host - MYSQL_PORT_3306_TCP_ADDR', default='marcdba')
 parser.add_argument('--mysql-port', help='database port - MYSQL_PORT_3306_TCP_PORT', default=3306)
+parser.add_argument('--db-user', required=True, help='database user', default='marcdba')
 parser.add_argument('--db-pass', required=True, help='database pw', default='deadbeef')
 
+parser.add_argument('database', help='database name')
 
 #
 # Databases to backup
