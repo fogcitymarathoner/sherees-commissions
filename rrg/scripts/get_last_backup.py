f5b5339c1db046ca03e5d76143f697a79847ae1d
marc condon <marc@fogtest.com>
2016-09-04 11:57:15 -0700

    dl last db bk

--- rrg/scripts/get_last_backup.py
+++ rrg/scripts/get_last_backup.py
@@ -2,14 +2,15 @@ import argparse
 from s3_mysql_backup import download_last_db_backup
 
 parser = argparse.ArgumentParser(description='RRG Get Last Backup')
+parser.add_argument('--db-backups-dir', help='database backups directory', default='/php-apps/db_backups/')
 
+parser.add_argument('--s3-folder',  help='S3 Folder', default='')
+parser.add_argument('--bucket-name', required=True, help='Bucket Name', default='php-apps-cluster')
+parser.add_argument('--aws-access-key-id', required=True, help='AWS_ACCESS_KEY_ID', default='rrg')
+parser.add_argument('--aws-secret-access-key', required=True, help='AWS_SECRET_ACCESS_KEY_ID', default='deadbeef')
+parser.add_argument('--backup-aging-time', help='delete backups older than backup-aging-time', default=30)
 
-parser.add_argument('project', help='project name',
-                    choices=['rrg', 'biz'])
-parser.add_argument(
-    '--db_backups_dir', required=True,
-    help='datadir dir with ar.xml',
-    default='backups')
+parser.add_argument('database', help='database name')
 
 
 def get_last_db_backup():
@@ -17,5 +18,5 @@ def get_last_db_backup():
     download last project db backup from S3
     """
     args = parser.parse_args()
-    download_last_db_backup(
-        db_backups_dir=args.db_backups_dir, project_name=args.project)
+    args.pat = "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-%s.sql.bz2" % args.database
+    download_last_db_backup(args)
