b90d4379da87f0b021e9b64f25b11d856fbded38
marc condon <marc@fogtest.com>
2016-10-14 10:07:17 -0700

    fixing commissions reports

--- rrg/scripts/commissions_monthly_summaries.py
+++ rrg/scripts/commissions_monthly_summaries.py
@@ -3,17 +3,17 @@ import argparse
 from tabulate import tabulate
 
 from rrg.models import session_maker
+from rrg.models import Employee
 from rrg.sherees_commissions import employee_year_month_statement
 from rrg.sherees_commissions import comm_months
 from rrg.utils import monthy_statement_ym_header
 
-parser = argparse.ArgumentParser(description='RRG Sherees Monthly Commissions Reports')
+parser = argparse.ArgumentParser(description='RRG All Sales People Monthly Commissions Reports')
 
 parser.add_argument(
     '--datadir', required=True,
     help='datadir dir with ar.xml',
-    default='/php-apps/cake.rocketsredglare.com/rrg/data/transactions/'
-            'invoices/invoice_items/commissions_items/')
+    default='/php-apps/cake.rocketsredglare.com/rrg/data/transactions/invoices/invoice_items/commissions_items/')
 
 parser.add_argument('--db-user', required=True, help='database user', default='marcdba')
 parser.add_argument('--mysql-host', required=True, help='database host - MYSQL_PORT_3306_TCP_ADDR', default='marcdba')
@@ -27,19 +27,22 @@ parser.set_defaults(cache=True)
 
 def monthlies_summary():
     args = parser.parse_args()
-
     session = session_maker(args)
-    balance = 0
-    for cm in comm_months(end=dt.now()):
-        print(monthy_statement_ym_header % (cm['month'], cm['year']))
-        args.year = cm['year']
-        args.month = cm['month']
-        total, res = employee_year_month_statement(session, args)
-        balance += total
-        res_dict_transposed = {
-            'id': [''],
-            'date': ['%s/%s' % (cm['month'], cm['year'])],
-            'description': ['New Balance: %s' % balance],
-            'amount': ['Period Total %s' % total]
-        }
-        print(tabulate(res_dict_transposed, headers='keys', tablefmt='plain'))
+    salespeople = session.query(Employee).filter(Employee.salesforce==True)
+    for salesperson in salespeople:
+        balance = 0
+        for cm in comm_months(end=dt.now()):
+            print('%s %s' % (salesperson.firstname, salesperson.lastname))
+            print(monthy_statement_ym_header % (cm['month'], cm['year']))
+            year = cm['year']
+            month = cm['month']
+            # employee_year_month_statement(session, employee, datadir, year, month, cache)
+            total, res = employee_year_month_statement(session, salesperson, args.datadir, year, month, args.cache)
+            balance += total
+            res_dict_transposed = {
+                'id': [''],
+                'date': ['%s/%s' % (cm['month'], cm['year'])],
+                'description': ['New Balance: %s' % balance],
+                'amount': ['Period Total %s' % total]
+            }
+            print(tabulate(res_dict_transposed, headers='keys', tablefmt='plain'))
