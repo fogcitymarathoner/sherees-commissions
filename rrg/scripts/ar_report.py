1b6895ed4024ad8dac24a11277897bfbada15fe8
marc condon <marc@fogtest.com>
2017-02-05 21:39:13 +0000

    checking in server-side progress on Flask-Scripting

--- rrg/scripts/ar_report.py
+++ rrg/scripts/ar_report.py
@@ -3,12 +3,29 @@ import os
 import argparse
 import xml.etree.ElementTree as ET
 
+from flask_script import Manager
+from flask import Flask
 from rrg.helpers import read_inv_xml_file
 from rrg.models import invoice_archives
 from rrg.utils import clients_ar_xml_file
-from flask_script import Manager
 
-from rrg import app
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the default configuration
+if os.environ.get('RRG_SETTINGS'):
+    settings_file = os.environ.get('RRG_SETTINGS')
+else:
+    print('Environment Variable RRG_SETTINGS not set')
+    quit(1)
+
+if os.path.isfile(settings_file):
+    try:
+        app.config.from_envvar('RRG_SETTINGS')
+    except Exception as e:
+        print('something went wrong with config file %s' % settings_file)
+        quit(1)
+else:
+    print('settings file %s does not exits' % settings_file)
 
 parser = argparse.ArgumentParser(description='RRG Accounts Receivable Reports')
 parser.add_argument('type', help='report type',
