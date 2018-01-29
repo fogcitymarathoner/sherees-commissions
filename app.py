103412f1473e0e17c8779f8401978326d8334386
fogcitymarathoner <marc@fogtest.com>
2018-01-28 16:10:45 -0800

    removed dependency on flask-alembic

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 """This is only used with the manage.py"""
 from flask import Flask
-from flask_alembic import Alembic
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,8 +10,6 @@ engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 sessionMaker = sessionmaker(bind=engine)
 session = sessionMaker()
 db = SQLAlchemy(app)
-alembic = Alembic()
-alembic.init_app(app)
 
 
 if __name__ == '__main__':
